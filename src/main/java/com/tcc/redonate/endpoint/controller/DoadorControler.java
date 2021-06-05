afa7a5613e31a653a379bbcdb247f3abf00f70cf
Lucas Vinicius Sousa Silva <lucas2606.vinicius@gmail.com>
2021-06-05 20:56:50 -0300

    Retirando código inutilizado

--- src/main/java/com/tcc/redonate/endpoint/controller/DoadorControler.java
+++ src/main/java/com/tcc/redonate/endpoint/controller/DoadorControler.java
@@ -3,25 +3,14 @@ package com.tcc.redonate.endpoint.controller;
 import com.tcc.redonate.endpoint.service.DoadorService;
 import com.tcc.redonate.model.Doador;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/listarDoadores")
-@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DoadorControler {
     private final DoadorService doadorService;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Iterable<Doador>> list(Pageable pageable){
-        return new ResponseEntity<>(doadorService.list(pageable), HttpStatus.OK);
-    }
 }
