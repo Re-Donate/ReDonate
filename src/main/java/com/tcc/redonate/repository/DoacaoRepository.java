22e2d697fc33e82bf04a01419d2d08f9e7e5317e
Lucas Vinicius Sousa Silva <lucas2606.vinicius@gmail.com>
2021-06-29 18:28:38 -0300

    Eliminando método desnecessário

--- src/main/java/com/tcc/redonate/repository/DoacaoRepository.java
+++ src/main/java/com/tcc/redonate/repository/DoacaoRepository.java
@@ -3,8 +3,5 @@ package com.tcc.redonate.repository;
 import com.tcc.redonate.model.Doacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DoacaoRepository extends JpaRepository<Doacao, Long> {
-    List<Doacao> findByIdInstituicao(Long idInstituicao);
 }
